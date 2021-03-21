612438e1af00e12d71af15c91b168827d4cea692
Rainfire267 <ejimenez267@gmail.com>
2021-03-21 02:48:53 +0000

    domain generator v3

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".es"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const getRandomNumber = max => Math.floor(Math.random() * max);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const getRandomName = () =>
+    `${pronoun[getRandomNumber(pronoun.length)]} ${
+      adj[getRandomNumber(adj.length)]
+    } ${noun[getRandomNumber(noun.length)]} ${
+      domain[getRandomNumber(domain.length)]
+    }`;
+
+  const setRandomName = () => {
+    document.getElementById("random-name").innerText = getRandomName();
+  };
+
+  document.getElementById("generate").addEventListener("click", setRandomName);
+
+  setRandomName();
 };
